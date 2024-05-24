6e4a64201422eb6dda178b9c180d4aab8eb0cf50
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-05-24 18:02:36 +0900

    chore: Update package description and dependencies

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="rottentomatoes-scraper",
-    version="0.0.1",
+setup(
+    name="rottentomatoes-review-scraper",
+    version="0.1",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "requests",
+        "pandas",
+        "selenium"
+    ],
+    entry_points={
+        'console_scripts': [
+            'rottentomatoes-scraper=main:main',
+        ],
+    },
     author="Fami Ishikawa",
     author_email="s2222061@stu.musashino-u.ac.jp",
-    description="A package to scrape Rotten Tomatoes",
-    long_description=long_description,
+    description="A Python package to scrape movie data from Rotten Tomatoes, including titles and reviews.",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/HwaI12/rottentomatoes-scraper",
-    project_urls={
-        "Bug Tracker": "https://github.com/HwaI12/rottentomatoes-scraper",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    py_modules=[
-        'rottentomatoes_scraper'
-    ],
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
-    entry_points={
-        'console_scripts': [
-            'rottentomatoes-scraper=rottentomatoes_scraper:main',
-        ],
-    },
+    python_requires='>=3.6',
 )
